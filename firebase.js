b363dcc10024740c25000f1195c06457b70ad867
vishisht-dubey <vishisht.dubey.phe21@itbhu.ac.in>
2023-07-11 18:47:04 +0530

    validation in pending

---
+++ firebase.js
@@ -0,0 +1,13 @@
+import {initializeApp} from "firebase/app"
+
+export const firebaseConfig = initializeApp({
+  apiKey: "AIzaSyDgyAIfggPO_5zYN6BqPNryqqPbv53gRBY",
+  authDomain: "ecell-assessment.firebaseapp.com",
+  projectId: "ecell-assessment",
+  storageBucket: "ecell-assessment.appspot.com",
+  messagingSenderId: "453122599404",
+  appId: "1:453122599404:web:0a7d6bfce3eed40882b8c9"
+});
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
